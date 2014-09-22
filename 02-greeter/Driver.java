96d0f0fcee9fd76b040ea6dc687625c44af49ba0
Lian Wong <ltaylor.wong@gmail.com>
2014-09-22 08:39:28 -0400

    String Things

--- 02-greeter/Driver.java
+++ 02-greeter/Driver.java
@@ -8,9 +8,20 @@ public class Driver {
 
 	//cant do cuz istance variable is private
 	//instead we can use a set method
-	g1.greeting=new String ("HEY");
-	g1.greet();
-	g2.greet();
+	g1.setGreeting(new String ("HEY"));
       
+	//here, we're using the string literal Hi there! and Java just returns
+	//it without grabbing a block of memory
+	g2.setGreeting("Hi there!");
+	
+	System.out.println(g1.greet());
+	System.out.println(g2.greet());
+
+
+	String s =g1.getGreeting();
+	System.out.println("g1's greeting is: "+s);
+	System.out.println("g2's greeting is: "+g2.getGreeting());
+	
+	
     }
 }
--- 02-greeter/greeter.java
+++ 02-greeter/greeter.java
@@ -5,14 +5,41 @@ public class greeter{
     //instance variables are almost always private
     //get access them with "accessor methods" get and set
     private String greeting =new String("HIIIIIIIIIIIIIIIIIII");
+    /*Notes -the name is the same as the class name
+            - there is no return type
+	    -its ALWAYS public
+
+	    a constuctor is run automartically when you instantiate teh class,
+	    that is, if you have: greeter g=new greeter("Hello")
+	    then Java will automatically call this constructor sending Hello
+	    in as parameter f. It will then set the instance variable
+	    greeting to what g refers to.
+     */
+    //constructors
+    public greeter(String g){
+	System.out.println("In the constructor");
+	greeting =g
+    }
+
     
     public void setGreeting(String s){
 	greeting = s;
+	//s refers to the string from before
     }
     
-    public void greet(){
-	//String greeting = new String ("HEYYYYY");
-	System.out.println(greeting);
+    //String is the return type, the type of data that we will
+    //send back to the retun statement
+    //void will return nothing
+    public String getGreeting(){
+	return greeting;
+    }
+    
+    /* our original greet method just printed the greeting
+       generall we want our methods to return information
+     */
+    
+    public String greet(){
+	return greeting;
     }
 
     public void ungreet(){
