6a557298f80b58b79e5c15b83a5d708cd47f6fff
roguester18 <ltaylor.wong@gmail.com>
2014-09-22 20:23:54 -0400

    HW #4

---
+++ 04-String stuff/Driver.java
@@ -0,0 +1,17 @@
+public String makeOutWord(String out, String word) {
+  String s=out.substring(0,2);
+  return s + word + out.substring (2, out.length());
+}
+public String firstHalf(String str) {
+  int s=str.length()/2;
+  return str.substring(0,s);
+}
+public boolean mixStart(String str) {
+  if (str.length() < 3) return false;
+  String s = str.substring(1, 3);
+  if (two.equals("ix")) {
+    return true;
+  } else {
+    return false;
+  }
+}
