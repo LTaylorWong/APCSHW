9dc901a5f22dc1fa9793482df0f353bd670dbac7
Lian Wong <ltaylor.wong@gmail.com>
2014-10-06 08:10:54 -0400

    shapes

--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -30,5 +30,27 @@ public class Shapes{
 	}
 	return s;
     }
-}
+
+
+    public String tri3(int h){
+	
+
+
+
+	/*
+
+_ _ * _ _   a = 2, b=1, a=2  row1   (5-1)/2= (length-row)/2
+_ * * * _   a=1, b=3, a=1    row2   
+* * * * *   a=0, b=5, a=0    row3
 		
+n = 3 --> 5
+n = 4 --> 7
+
+length = 2n-1
+
+
+1+2+2+2...
+2r+1
+row1 i=1
+row2 i=3
+row3 i=5
