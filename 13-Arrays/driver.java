7e8c11a8693138f92f660e1089b95de9def3d989
roguester18 <ltaylor.wong@gmail.com>
2014-10-24 00:26:23 -0400

    Arrays

---
+++ 13-Arrays/driver.java
@@ -0,0 +1,9 @@
+public class driver{
+    public static void main(String[] args){
+	f=new frontPiece();
+	s=new sum13();
+	System.out.println(frontPiece({1,2,3});
+	System.out.println(sum13({1,2,2,1});
+	System.out.println(sum13({1,2,13,2,1});
+    }
+}
