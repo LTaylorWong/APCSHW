fcb06bbfbb0696a28f26d079541611e258eee37b
roguester18 <ltaylor.wong@gmail.com>
2014-10-29 22:08:19 -0400

    more array things

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -28,4 +28,57 @@ public class Arraystuff{
 	return x;
     }
     
-   
+    public int sum67(int[] nums) {
+	int sum = 0;
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums[i] != 6) sum += nums[i];
+	    else
+		while (nums[i] != 7) i++;
+	}
+	return sum;
+    }   
+
+    public boolean more14(int[] nums) {
+	int ones = 0;
+	int fours = 0;
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums[i] == 1) ones=ones+1;
+	    if (nums[i] == 4) fours=fours+1;
+	}
+	return ones > fours;
+    }
+
+    public boolean tripleUp(int[] nums) {
+	for (int i = 0; i <= nums.length - 3; i++)
+	    if (nums[i + 1] == nums[i] + 1 && nums[i + 2] == nums[i] + 2)
+		return true;
+	return false;
+    }
+
+    public int[] tenRun(int[] nums) {
+	boolean replace = false;
+	int x = 0; 
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums[i] % 10 == 0) {
+		if (!replace) {
+		    replace = true;
+		    x= nums[i];
+		} else
+		    x= nums[i];
+	    }
+	    if (nums[i] % 10 != 0 && replace) nums[i] = x;
+	}
+	return nums;
+    }
+
+    public int freq(x){
+	int counter=0;
+	y=a[x];
+	for (int i=0;i<a.length;i++){
+	    if(a[i]==y){
+		counter=counter+1;
+	    }
+	}
+	return counter;
+    }
+
