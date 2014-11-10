5987bfda09d7fcb733a8a2b4c96db296049b4cc5
roguester18 <ltaylor.wong@gmail.com>
2014-11-09 22:37:22 -0500

    supa array

--- 15-SuperArray/Sarray.java
+++ 15-SuperArray/Sarray.java
@@ -1,41 +1,72 @@
 public class Sarray{
-    private int[] data; //should be object()
-    private int location;
+    private int[] data;
+    private int last;
+    
     public Sarray(){
 	//start array at size 10
 	data = new int[10];
-	location = 0;
+	last = 0;
     }
     //tells if array is full or not
     public boolean isFull(){
-	return (location>=data.length);
+	return (size()=data.length);
     }
     //add to the end
-    public void addToEnd(int i){
+    public boolean add(int i){
 	if (isFull()){
+	    int[] ret = new int[data.length+1];
+	    for (int y=0;y<data.length;y++){
+		ret[y]=data[y];
+	    }
 	}else{
-	    set (location, i);
+	    set (last, i);
+	}
+	return true;
     }
     //add to a location and shift everything down
-    public void addToLoc(int index, int i){
-
+    public void add(int index, int i){
+	int[] ret = new int[last+1];
+	for (int y=0;y<ret.length; y++){
+	    if (y < index){
+		ret[y]=data[y];
+	    }else if (i== index){
+		ret[y]=i;
+	    }else{
+		ret[y]=data[x-1];
+	    }
+	}
+	last=last+1;
+	data=ret;
     }
     //get int index
     public int get(int index){
+	return data[index];
     }
-    //set in index
-    public void set(int index, int i){
-	if (index>location){
-	    location = index +1;
-	}else{ 
-	location++;
+    //sets the item at location index to value 1
+    public int set(int index, int i){
+        int x = data[index];
+	data[index]=i;
+	return x;
     }
+
     //size of the set
     public int size(){
-	return data.length;
+	return last;
     }
-    //remove
-    public void remove(int index){
+    //removes item at index 
+    public int remove(int index){
+	int[] ret = new int[data.length -1];
+	for (int i=0:i<ret.lenth;i++){
+	    if (i<index){
+		ret[i]=data[i];
+	    }else if (i>index){
+		ret[i]=data[i+1];
+	    }else {
+		old = ret[i];
+	    }
+	}
+	last = last -1;
+	data = ret;
+	return old;
     }
-    //grows array
-    public void grow()
+}
---
+++ 15-SuperArray/Sarray.java~
@@ -0,0 +1,41 @@
+public class Sarray{
+    private int[] data; //should be object()
+    private int location;
+    public Sarray(){
+	//start array at size 10
+	data = new int[10];
+	location = 0;
+    }
+    //tells if array is full or not
+    public boolean isFull(){
+	return (location>=data.length);
+    }
+    //add to the end
+    public void addToEnd(int i){
+	if (isFull()){
+	}else{
+	    set (location, i);
+    }
+    //add to a location and shift everything down
+    public void addToLoc(int index, int i){
+
+    }
+    //get int index
+    public int get(int index){
+    }
+    //set in index
+    public void set(int index, int i){
+	if (index>location){
+	    location = index +1;
+	}else{ 
+	location++;
+    }
+    //size of the set
+    public int size(){
+	return data.length;
+    }
+    //remove
+    public void remove(int index){
+    }
+    //grows array
+    public void grow()
