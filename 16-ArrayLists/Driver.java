81daafa431e157e772a120b5b20f1f75dc775c42
roguester18 <ltaylor.wong@gmail.com>
2014-11-13 08:09:27 -0500

    Update Driver.java

--- 16-ArrayLists/Driver.java
+++ 16-ArrayLists/Driver.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Driver{
     public static void main(String[] args){
-	ArrayList<Interger> i;
+	ArrayList<Integer> i;
 	i = new ArrayList<Integer>();
 	Random r = new Random();
 	for(int x=0;x<20;x++){
