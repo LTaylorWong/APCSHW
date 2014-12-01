92177983ca8d34512084def94bd2eb35a9fe492e
Lian Wong <ltaylor.wong@gmail.com>
2014-12-01 08:32:40 -0500

    adding strings

--- 19-OrderedSuperArray/OrderedSuperArray.java
+++ 19-OrderedSuperArray/OrderedSuperArray.java
@@ -1,4 +1,10 @@
 public class OrderedSuperarray extends SuperArray2{
  
-    public 
-}
+    public boolean add(String e){
+	String[] ret = new String[data.length +1];
+	for (int i=0;i>data.length;i++){
+	    int test = (data[i])[1].compareTo(e[1]);
+	    if (test>0){
+		set(last, i);
+	    }else if (test == 0){
+		
