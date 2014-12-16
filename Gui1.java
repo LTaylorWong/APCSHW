f5d1f4adf58c6a857aba30889e69672a762f10ab
Lian Wong <ltaylor.wong@gmail.com>
2014-12-16 08:34:32 -0500

    GUI

--- Gui1.java
+++ Gui1.java
@@ -3,16 +3,48 @@ import java.awt.*;
 
 public class Gui1 extends JFrame {
 
-		public Gui1(){
-
-				setTitle("My First Gui");
-				setSize(600,400);
-				setLocation(100,100);
-				setDefaultCloseOperation(EXIT_ON_CLOSE);
+    private Container pane;
+    private JButton b1,b2;
+    private JLabel l;
+    private JTextArea text;
+    private JPanel canvas;
+    
+    public Gui1(){
+	
+	setTitle("My First Gui");
+	setSize(600,400);
+	setLocation(100,100);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	pane=getContentPane();
+	pane.setLayout(new FlowLayout());
+	l=new JLabel("The Labels");
+	pane.add(l);
+	b1=new JButton("button");
+	pane.add(b1);
+	b2=new JButton("exit");
+	pane.add(b2);
+	text=new JTextArea();
+	text.setColumns(40);
+	text.setRows(10);
+	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	pane.add(text);
+	canvas=new JPanel();
+	canvas.setPreferredSize(new Dimension(300,300));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
+	pane.add(canvas);
+	
+	/*
+	pane.add(new JButton("hello1"));
+	pane.add(new JButton("hello2"));
+	pane.add(new JButton("hello3"));
+	JPanel f2= new JPanel();
+	pane.add(f2);
+	*/
 				
-		}
-		public static void main(String[] args) {
-			  Gui1 f = new Gui1();
-				f.setVisible(true);
-		}
+    }
+    public static void main(String[] args) {
+	Gui1 f = new Gui1();
+	f.setVisible(true);
+    }
 }
